fb8d4ba151582108b0a267ce9d97de9b19123d86
amit7321 <amit.hasan7321@gmail.com>
2024-10-20 22:37:02 +0600

    Implement Tag add.

--- Controllers/AdminTagsController.cs
+++ Controllers/AdminTagsController.cs
@@ -1,3 +1,6 @@
+using System.ComponentModel.DataAnnotations;
+using Blog_web_app.Data;
+using Blog_web_app.Models.Domain;
 using Blog_web_app.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
@@ -5,6 +8,12 @@ namespace Blog_web_app.Controllers;
 
 public class AdminTagsController : Controller
 {
+    private readonly BlogDbContext blogDbContext;
+    public AdminTagsController(BlogDbContext blogDbContext)
+    {
+        this.blogDbContext = blogDbContext;
+    }
+
     [HttpGet]
     public IActionResult Add()
     {
@@ -15,12 +24,21 @@ public class AdminTagsController : Controller
     // [ActionName("Add")]
     public IActionResult Add(AddTagRequest addTagRequest)
     {
-        var name = addTagRequest.Name;
-        var displayName = addTagRequest.DisplayName;
+        var tag = new Tag
+        {
+            Name = addTagRequest.Name,
+            DisplayName = addTagRequest.DisplayName
+        };
+
+        blogDbContext.Tags.Add(tag);
+        blogDbContext.SaveChanges();
+
+        // var name = addTagRequest.Name;
+        // var displayName = addTagRequest.DisplayName;
 
         /* var name = Request.Form["name"];
         var displayName = Request.Form["displayName"]; */
 
         return View("Add");
-    }
+    } 
 }
